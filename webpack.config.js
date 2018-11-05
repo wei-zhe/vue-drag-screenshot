4a8104128b8f0fae8a66d1ce3c988c9f9bafe224
WeiZhe <2270734250@qq.com>
2018-11-05 10:28:12 +0800

    require名修改

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     publicPath: '/dist/',
    // filename: 'build.js' // 打包后输出的文件名
     filename: 'vue-drag-screenshot.js', // 我们可不想打包后叫build.js 多low啊 起一个与项目相对应的
-    library: 'PayKeyboard', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
+    library: 'DragScreenshot', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
     libraryTarget: 'umd', //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
   },
